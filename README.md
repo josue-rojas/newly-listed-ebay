0bb91aac6c24f8fda77864c3510d0ff996aad656
josue rojas <josuerojas.rojas@gmail.com>
2018-03-26 18:35:08 -0400

    added options; ability to message (mac only); ability to change url in terminal; ability to change sleep_time

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ A script that constantly checks for new items (provided by a url for now)
 - beautifulsoup4
 - requests
 
+### Things used (credit & cool things)
+- [AppleScript to send messages (only tested with imessage)](https://stackoverflow.com/questions/11812184/how-to-send-an-imessage-text-with-applescript-only-in-provided-service/19483011#19483011)
+
 ### TODO
 - http://www.helios825.org/url-parameters.php ADD SUPPORT FOR CUSTOM URL IN INPUT
 - option for alert or open
--
+- maybe change to use the ebay api instead!?!?! [here](https://developer.ebay.com/Devzone/finding/CallRef/findItemsAdvanced.html)(but then it would need keys)
--- script.py
+++ script.py
@@ -2,38 +2,80 @@ import webbrowser
 from bs4 import BeautifulSoup
 import requests
 import time
-
-url = 'https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_ipg=50%27&_nkw=yeezy+boost+350+v2&_sop=10'
+import os
+import getopt
+import sys
 
 #i use a set to keep track of the urls (ebay urls sometimes change so i think its best to keep the whole url)
 seen = set([])
 
 # returns list of <a class='vip'></a>
-def getLinks():
+def getLinks(url):
     r = requests.get(url).content
     soup = BeautifulSoup(r)
     return soup.find_all('a', {'class':'vip'})
 
 
-
-def main():
-    for link in getLinks():
-        itm_link = link.get('href')
-        if not itm_link in seen:
-            seen.add(itm_link)
-            print(itm_link)
+def newListing(url, message, phone_num, sleep_time):
     while(True):
-        time.sleep(60)
+        time.sleep(sleep_time)
         print('Checking....')
-        for link in getLinks():
+        for link in getLinks(url):
             itm_link = link.get('href')
             if not itm_link in seen:
                 print(itm_link)
                 webbrowser.open(itm_link)
                 seen.add(itm_link)
+                if message:
+                    print('osascript message.scpt '+ phone_num + ' "' + itm_link +'"')
+                    os.system('osascript message.scpt '+ phone_num + ' "' + itm_link +'"')
             else:
-                print('None found...')
+                print('No more found...')
                 break
+
+def usage():
+    print('Usage:\n...-m --message: flag to allow send message of new founding\n-p --phone: phone number to send the message (you can also change defaults in python file instead)\n-u --url: ebay url of new listings (it is much easier to change defaul in py file (also make sure url is for new listings))\n-s --sleep: change default sleep time, default is 60sec (or change in py file)')
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hmp:u:s:', ['help', 'message','phone=', 'url=', 'sleep='])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(2)
+
+    # ***************************
+    # defaults, change them to your liking or use the command line
+    url = 'https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_ipg=50%27&_nkw=yeezy+boost+350+v2&_sop=10'
+    phone_num = ''
+    sleep_time = 60
+    message = False
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            exit()
+        elif o in ('-m', '-message'):
+            message = True
+        elif o in ('-p', '-phone'):
+            phone_num = a
+        # url is better to change default in this file rather than have an INPUT
+        # else you have to escape the characters that interfere like '\' or '&'
+        elif o in ('-u', '-url'):
+            url = a
+        elif o in ('-s', '-sleep'):
+            sleep_time = a
+        else:
+            print 'unhandled option'
+
+    # first run should be ignored (i mean unless you want to be notified of a bunch of stuff)
+    for link in getLinks(url):
+        itm_link = link.get('href')
+        if not itm_link in seen:
+            seen.add(itm_link)
+            print(itm_link)
+    # while
+    newListing(url, message, phone_num, sleep_time);
 
 
 if __name__ == "__main__":
